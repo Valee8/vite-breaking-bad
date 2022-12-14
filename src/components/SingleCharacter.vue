83a90e16c30a4026ad33f66e7d08d42f76df9eaf
Vale <oirelav95@gmail.com>
2022-12-14 17:05:51 +0100

    creo componente SingleCharacter

---
+++ src/components/SingleCharacter.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    name: "SingleCharacter",
+    props: ["info"]
+}
+</script>
+
+<template>
+
+    <img :src="info.image" :alt="info.name">
+
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
+</style>
