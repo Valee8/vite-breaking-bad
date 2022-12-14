f2d67128cf4b996d68298c47ad45dc39e407cc84
Vale <oirelav95@gmail.com>
2022-12-14 17:05:03 +0100

    Creo componente AppHeader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+    name: "AppHeader",
+    data() {
+        return {
+            title: 'Rick and Morty Api'
+        }
+    }
+}
+
+</script>
+
+<template>
+
+    <header>
+        <h1>
+            {{ title }}
+        </h1>
+    </header>
+
+</template>
+
+<style lang="scss" scoped>
+header {
+    padding: 10px;
+
+    h1 {
+        color: #F8F9FA;
+        text-align: left;
+    }
+}
+</style>
