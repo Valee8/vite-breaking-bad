ce67a37264b6733bdd1eaeb5ec576e1512f6ead9
Vale <oirelav95@gmail.com>
2022-12-14 17:05:14 +0100

    creo componente AppMain

---
+++ src/components/AppMain.vue
@@ -0,0 +1,33 @@
+<script>
+
+import SingleCharacter from './SingleCharacter.vue'
+
+import { store } from '../store.js'
+
+export default {
+    name: "CharactersList",
+    components: {
+        SingleCharacter,
+    },
+    data() {
+        return {
+            store,
+        }
+    }
+}
+
+</script>
+
+<template>
+
+    <section>
+        <div v-for="character in store.characterList" :key="character.id">
+            <SingleCharacter :info="character" />
+        </div>
+    </section>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
