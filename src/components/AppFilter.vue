b541b17de7ff450642b99f6d7397e661b8c2e8be
Vale <oirelav95@gmail.com>
2022-12-15 15:31:17 +0100

    Aggiungo select per filtrare i personaggi

---
+++ src/components/AppFilter.vue
@@ -0,0 +1,39 @@
+<script>
+
+import { store } from '../store';
+
+export default {
+    name: "AppFilter",
+    data() {
+        return {
+            store,
+            options: [
+                {
+                    value: ""
+                },
+                {
+                    value: "alive"
+                },
+                {
+                    value: "dead"
+                },
+                {
+                    value: "unknown"
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<template>
+
+    <select v-model="store.select" @click="$emit('filter')">
+        <option v-for="(option, index) in options" :value="option.value" :key="index">{{ option.value }}</option>
+    </select>
+
+</template>
+
+<style lang="scss">
+@use '../styles/general.scss' as *;
+</style>
