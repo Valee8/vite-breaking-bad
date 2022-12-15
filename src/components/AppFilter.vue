a18ccd70e766a1142608d80b578bdeebfefd26c3
Vale <oirelav95@gmail.com>
2022-12-15 18:04:44 +0100

    modifica valori array options

--- src/components/AppFilter.vue
+++ src/components/AppFilter.vue
@@ -9,16 +9,29 @@ export default {
             store,
             options: [
                 {
-                    value: ""
+                    value: "",
+                    disabled: true,
+                    text: "Select category"
                 },
                 {
-                    value: "alive"
+                    value: "",
+                    disabled: false,
+                    text: ""
                 },
                 {
-                    value: "dead"
+                    value: "alive",
+                    disabled: false,
+                    text: "alive"
                 },
                 {
-                    value: "unknown"
+                    value: "dead",
+                    disabled: false,
+                    text: "dead"
+                },
+                {
+                    value: "unknown",
+                    disabled: false,
+                    text: "unknown"
                 }
             ]
         }
@@ -30,8 +43,9 @@ export default {
 
     <div class="container">
         <select v-model="store.select" @click="$emit('filter')">
-            <option disabled value="">Select category</option>
-            <option v-for="(option, index) in options" :value="option.value" :key="index">{{ option.value }}</option>
+            <option :disabled="option.disabled" v-for="(option, index) in options" :value="option.value" :key="index">{{
+                    option.text
+            }}</option>
         </select>
     </div>
 
