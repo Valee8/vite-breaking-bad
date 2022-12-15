194286e9d882f576d8691752fea8f20dc6de303a
Vale <oirelav95@gmail.com>
2022-12-15 17:38:13 +0100

    creo componente NumbersOfCharacters

---
+++ src/components/NumberOfCharacters.vue
@@ -0,0 +1,27 @@
+<script>
+import { store } from '../store';
+
+export default {
+    name: "NumberOfCharacters",
+    data() {
+        return {
+            store,
+        }
+    }
+}
+
+</script>
+
+
+<template>
+
+    <h1>
+        {{ store.characterList.length }}
+    </h1>
+
+</template>
+
+
+<style lang="scss">
+@use '../styles/general.scss' as *;
+</style>
